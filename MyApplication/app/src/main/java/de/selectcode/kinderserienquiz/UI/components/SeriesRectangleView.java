7f13a49c3bda629b3e3ef4b2957b0a61d98ffcd8
Stephan Le <stephan.le99@yahoo.de>
2017-06-06 15:46:25 +0200

    Modified SeriesRectangleView

---
+++ MyApplication/app/src/main/java/de/selectcode/kinderserienquiz/UI/components/SeriesRectangleView.java
@@ -0,0 +1,18 @@
+package de.selectcode.kinderserienquiz.UI.components;
+
+import android.content.Context;
+import android.support.v7.widget.ButtonBarLayout;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by steph on 06.06.2017.
+ */
+
+public class SeriesRectangleView extends android.support.v7.widget.AppCompatButton {
+
+    public SeriesRectangleView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
